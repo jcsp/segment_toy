9cb68018cbd4786a17a524638ba7fab5bfac2e61
John Spray <jcs@redpanda.com>
2023-05-09 11:09:41 +0100

    Update bucket_reader for archive manifests

--- src/bucket_reader.rs
+++ src/bucket_reader.rs
@@ -37,6 +37,9 @@ pub struct Anomalies {
     /// Segment objects not mentioned in their manifest
     pub segments_outside_manifest: Vec<String>,
 
+    /// Archive manifests not referenced by a head manifest
+    pub archive_manifests_outside_manifest: Vec<String>,
+
     /// PartitionManifest that could not be loaded
     pub malformed_manifests: Vec<String>,
 
@@ -103,6 +106,10 @@ impl Anomalies {
             &self.segments_outside_manifest,
         ));
         result.push_str(&Self::report_line(
+            "Archive manifests outside manifest",
+            &self.archive_manifests_outside_manifest,
+        ));
+        result.push_str(&Self::report_line(
             "Malformed partition manifests",
             &self.malformed_manifests,
         ));
@@ -141,6 +148,7 @@ impl Anomalies {
     fn new() -> Anomalies {
         Self {
             segments_outside_manifest: vec![],
+            archive_manifests_outside_manifest: vec![],
             malformed_manifests: vec![],
             malformed_topic_manifests: vec![],
             ntpr_no_manifest: HashSet::new(),
@@ -151,10 +159,39 @@ impl Anomalies {
     }
 }
 
+pub struct PartitionMetadata {
+    // This field is not logically optional for a well-formed partition's metadata, but is
+    // physically optional here because we may discover archive manifests prior to discovering
+    // the head manifest.
+    head_manifest: Option<PartitionManifest>,
+    archive_manifests: Vec<ArchivePartitionManifest>,
+
+}
+
+impl PartitionMetadata {
+    pub fn contains_segment(&self, seg: &SegmentObject) -> bool {
+        let shortname = format!("{}-{}-v1.log", seg.base_offset, seg.original_term);
+
+        if let Some(hm) = &self.head_manifest {
+            if hm.contains_segment_shortname(&shortname) {
+                return true;
+            }
+        }
+
+        for am in &self.archive_manifests {
+            if am.manifest.contains_segment_shortname(&shortname) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
 /// Find all the partitions and their segments within a bucket
 pub struct BucketReader {
     pub partitions: HashMap<NTPR, PartitionObjects>,
-    pub partition_manifests: HashMap<NTPR, PartitionManifest>,
+    pub partition_manifests: HashMap<NTPR, PartitionMetadata>,
     pub topic_manifests: HashMap<NTR, TopicManifest>,
     pub anomalies: Anomalies,
     pub client: Arc<dyn ObjectStore>,
@@ -213,6 +250,13 @@ impl BucketReader {
             } else if key.ends_with("/topic_manifest.json") {
                 debug!("Parsing topic manifest key {}", key);
                 self.ingest_topic_manifest(&key).await?;
+            } else if key.contains("manifest.json_") {
+                debug!("Parsing partition archive manifest key {}", key);
+                self.ingest_archive_manifest(&key).await?;
+            } else if key.ends_with(".index") {
+                // TODO: do something with index files: currently ignore them as they are
+                // somewhat disposable.
+                debug!("Ignoring index key {}", key);
             } else {
                 debug!("Parsing segment key {}", key);
                 self.ingest_segment(&key);
@@ -226,8 +270,15 @@ impl BucketReader {
         debug!("Loaded {} topic manifests", self.topic_manifests.len());
 
         for (ntpr, partition_objects) in &mut self.partitions {
-            let p_manifest_o = self.partition_manifests.get(ntpr);
-            if let None = p_manifest_o {
+            if ntpr.ntp.partition_id == 0 {
+                let t_manifest_o = self.topic_manifests.get(&ntpr.to_ntr());
+                if let None = t_manifest_o {
+                    self.anomalies.ntr_no_topic_manifest.insert(ntpr.to_ntr());
+                }
+            }
+
+            let p_metadata_o = self.partition_manifests.get(ntpr);
+            if p_metadata_o.is_none() {
                 // The manifest may be missing because we couldn't load it, in which
                 // case that is already tracked in malformed_manifests
                 let manifest_key = PartitionManifest::manifest_key(ntpr);
@@ -238,39 +289,37 @@ impl BucketReader {
                 }
             }
 
-            if ntpr.ntp.partition_id == 0 {
-                let t_manifest_o = self.topic_manifests.get(&ntpr.to_ntr());
-                if let None = t_manifest_o {
-                    self.anomalies.ntr_no_topic_manifest.insert(ntpr.to_ntr());
-                }
-            }
+            // Unwrap down to the manifest: if these were None we have
+            // already logged anomalies for that, so can just drop out.
+            let p_metadata = match p_metadata_o {
+                Some(p_m) => p_m,
+                None => { continue; }
+            };
+            let partition_manifest = match &p_metadata.head_manifest {
+                Some(p_m) => p_m,
+                None => { continue; }
+            };
 
-            if let Some(partition_manifest) = p_manifest_o {
-                // TODO: also mutate the lists of objects:
-                // - Drop segments that are outside the manifest, unless they are
-                //   at an offset higher than the tip of the manifest.
-                // - Drop segments that overlap: retain the one that is mentioned
-                //   in the manifest, or whichever appears to come from a newer term.
-                for o in &partition_objects.segment_objects {
-                    match &partition_manifest.segments {
-                        None => {
+            // TODO: also mutate the lists of objects:
+            // - Drop segments that are outside the manifest, unless they are
+            //   at an offset higher than the tip of the manifest.
+            // - Drop segments that overlap: retain the one that is mentioned
+            //   in the manifest, or whichever appears to come from a newer term.
+            for o in &partition_objects.segment_objects {
+                match &partition_manifest.segments {
+                    None => {
+                        self.anomalies.segments_outside_manifest.push(o.key.clone());
+                    }
+                    Some(segments) => {
+                        if !p_metadata.contains_segment(&o) {
                             self.anomalies.segments_outside_manifest.push(o.key.clone());
-                        }
-                        Some(segments) => {
-                            // Get short name of segment
-                            let shortname = format!("{}-{}-v1.log", o.base_offset, o.original_term);
-
-                            let found = segments.get(&shortname);
-                            if let None = found {
-                                self.anomalies.segments_outside_manifest.push(o.key.clone());
-                            }
                         }
                     }
                 }
             }
         }
 
-        for (ntpr, partition_manifest) in &self.partition_manifests {
+        for (ntpr, partition_metadata) in &self.partition_manifests {
             let mut known_objects: HashSet<String> = HashSet::new();
             if let Some(segment_objects) = self.partitions.get(&ntpr) {
                 for o in &segment_objects.segment_objects {
@@ -278,30 +327,43 @@ impl BucketReader {
                 }
             }
 
+            // We will validate the manifest.  If there is no head manifest, that is an anomaly.
+            let partition_manifest = match &partition_metadata.head_manifest {
+                Some(pm) => pm,
+                None => {
+                    // No head manifest: this is a partition for which we found archive
+                    // manifests but no head manifest.
+                    for am in &partition_metadata.archive_manifests {
+                        self.anomalies.archive_manifests_outside_manifest.push(am.key(ntpr))
+                    }
+                    continue;
+                }
+            };
+
             // For all segments in the manifest, check they were found in the bucket
             debug!(
-                "Checking {} ({} segments)",
-                partition_manifest.ntp(),
-                partition_manifest.segments.as_ref().map_or(0, | s | s.len())
-                );
+    "Checking {} ({} segments)",
+    partition_manifest.ntp(),
+    partition_manifest.segments.as_ref().map_or(0, | s | s.len())
+    );
             if let Some(manifest_segments) = &partition_manifest.segments {
                 for (segment_short_name, segment) in manifest_segments {
                     if let Some(so) = partition_manifest.start_offset {
                         if segment.committed_offset < so {
                             debug!(
-                                "Not checking {} {}, it is below start offset",
-                                partition_manifest.ntp(),
-                                segment_short_name
-                                );
+    "Not checking {} {}, it is below start offset",
+    partition_manifest.ntp(),
+    segment_short_name
+    );
                             continue;
                         }
                     }
 
                     debug!(
-                        "Checking {} {}",
-                        partition_manifest.ntp(),
-                        segment_short_name
-                        );
+    "Checking {} {}",
+    partition_manifest.ntp(),
+    segment_short_name
+    );
                     if let Some(expect_key) = partition_manifest.segment_key(segment) {
                         debug!("Calculated segment {}", expect_key);
                         if !known_objects.contains(&expect_key) {
@@ -364,8 +426,70 @@ impl BucketReader {
             let ntpr = NTPR {
                 ntp: NTP {
                     namespace: ns,
-                    topic: topic,
-                    partition_id: partition_id,
+                    topic,
+                    partition_id,
+                },
+                revision_id: partition_revision,
+            };
+            // TODO: I don't really want to surface the error here+now if it's
+            // transient: retry wrapper?
+            let path = object_store::path::Path::from(key);
+            let bytes = self.client.get(&path).await?.bytes().await?;
+
+            let manifest: PartitionManifest = match serde_json::from_slice(&bytes) {
+                Ok(m) => m,
+                Err(e) => {
+                    warn!("Error parsing JSON partition manifest {}: {}", key, e);
+                    self.anomalies.malformed_manifests.push(key.to_string());
+                    // This is OK because we cleanly logged anomaly.
+                    return Ok(());
+                }
+            };
+
+            // Note: assuming memory is sufficient for manifests
+            match self.partition_manifests.get_mut(&ntpr) {
+                Some(meta) => {
+                    meta.head_manifest = Some(manifest);
+                }
+                None => {
+                    self.partition_manifests.insert(ntpr, PartitionMetadata {
+                        head_manifest: Some(manifest),
+                        archive_manifests: vec![],
+                    });
+                }
+            }
+        } else {
+            warn!("Malformed partition manifest key {}", key);
+            self.anomalies.malformed_manifests.push(key.to_string());
+        }
+        Ok(())
+    }
+
+    async fn ingest_archive_manifest(&mut self, key: &str) -> Result<(), BucketReaderError> {
+        lazy_static! {
+            static ref PARTITION_MANIFEST_KEY: Regex =
+                Regex::new("[a-f0-9]+/meta/([^]]+)/([^]]+)/(\\d+)_(\\d+)/manifest.json_(\\d+)_(\\d+)_(\\d+)_(\\d+)_(\\d+)_(\\d+)").unwrap();
+        }
+        if let Some(grps) = PARTITION_MANIFEST_KEY.captures(key) {
+            // Group::get calls are safe to unwrap() because regex always has those groups if it matched
+            let ns = grps.get(1).unwrap().as_str().to_string();
+            let topic = grps.get(2).unwrap().as_str().to_string();
+            // (TODO: these aren't really-truly safe to unwrap because the string might have had too many digits)
+            let partition_id = grps.get(3).unwrap().as_str().parse::<u32>().unwrap();
+            let partition_revision = grps.get(4).unwrap().as_str().parse::<u64>().unwrap();
+
+            let base_offset = grps.get(5).unwrap().as_str().parse::<u64>().unwrap();
+            let committed_offset = grps.get(6).unwrap().as_str().parse::<u64>().unwrap();
+            let base_kafka_offset = grps.get(7).unwrap().as_str().parse::<u64>().unwrap();
+            let next_kafka_offset = grps.get(8).unwrap().as_str().parse::<u64>().unwrap();
+            let base_ts = grps.get(9).unwrap().as_str().parse::<u64>().unwrap();
+            let last_ts = grps.get(10).unwrap().as_str().parse::<u64>().unwrap();
+
+            let ntpr = NTPR {
+                ntp: NTP {
+                    namespace: ns,
+                    topic,
+                    partition_id,
                 },
                 revision_id: partition_revision,
             };
@@ -375,14 +499,39 @@ impl BucketReader {
             let bytes = self.client.get(&path).await?.bytes().await?;
 
             // Note: assuming memory is sufficient for manifests
-            debug!("Storing manifest for {} from key {}", ntpr, key);
-            if let Ok(manifest) = serde_json::from_slice(&bytes) {
-                if let Some(_) = self.partition_manifests.insert(ntpr, manifest) {
-                    warn!("Two manifests for same NTPR seen ({})", key);
+            debug!("Storing archive manifest for {} from key {}", ntpr, key);
+
+            let manifest: PartitionManifest = match serde_json::from_slice(&bytes) {
+                Ok(m) => m,
+                Err(e) => {
+                    warn!("Error parsing JSON partition archive manifest {}: {}", key, e);
+                    self.anomalies.malformed_manifests.push(key.to_string());
+                    // This is OK because we cleanly logged anomaly.
+                    return Ok(());
                 }
-            } else {
-                warn!("Error parsing JSON partition manifest {}", key);
-                self.anomalies.malformed_manifests.push(key.to_string());
+            };
+
+            let archive_manifest = ArchivePartitionManifest {
+                manifest,
+                base_offset,
+                committed_offset,
+                base_kafka_offset,
+                next_kafka_offset,
+                base_ts,
+                last_ts,
+            };
+
+            // Note: assuming memory is sufficient for manifests
+            match self.partition_manifests.get_mut(&ntpr) {
+                Some(meta) => {
+                    meta.archive_manifests.push(archive_manifest);
+                }
+                None => {
+                    self.partition_manifests.insert(ntpr, PartitionMetadata {
+                        head_manifest: None,
+                        archive_manifests: vec![archive_manifest],
+                    });
+                }
             }
         } else {
             warn!("Malformed partition manifest key {}", key);
@@ -524,6 +673,48 @@ pub struct PartitionManifest {
     insync_offset: Option<u64>,
     last_uploaded_compacted_offset: Option<u64>,
     start_offset: Option<u64>,
+}
+
+impl PartitionManifest {
+    pub fn contains_segment_shortname(&self, short_name: &str) -> bool {
+        if let Some(segs) = &self.segments {
+            segs.contains_key(short_name)
+        } else {
+            false
+        }
+    }
+}
+
+/// Metadata spilled from the head partition manifest: this includes a full manifest of
+/// its own, plus additional fields that are encoded in the key
+pub struct ArchivePartitionManifest {
+    manifest: PartitionManifest,
+    base_offset: u64,
+    committed_offset: u64,
+    base_kafka_offset: u64,
+    next_kafka_offset: u64,
+    base_ts: u64,
+    last_ts: u64,
+}
+
+impl ArchivePartitionManifest {
+    pub fn key(&self, ntpr: &NTPR) -> String {
+        let path = format!(
+            "{}/{}/{}_{}",
+            ntpr.ntp.namespace, ntpr.ntp.topic, ntpr.ntp.partition_id, ntpr.revision_id
+        );
+        let bitmask = 0xf0000000;
+        let hash = xxh32(path.as_bytes(), 0);
+        format!("{:08x}/meta/{}/manifest.json_{}_{}_{}_{}_{}_{}",
+                hash & bitmask, path,
+                self.base_offset,
+                self.committed_offset,
+                self.base_kafka_offset,
+                self.next_kafka_offset,
+                self.base_ts,
+                self.last_ts
+        )
+    }
 }
 
 impl PartitionManifest {
